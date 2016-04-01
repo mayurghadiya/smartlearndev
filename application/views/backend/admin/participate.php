73206bbc8d3752fac09b65bb2bb422e31f3b898c
mayur <mayur@localhost>
2016-04-01 18:23:40 +0530

    change date format of "date of submission"

--- application/views/backend/admin/participate.php
+++ application/views/backend/admin/participate.php
@@ -146,7 +146,7 @@
 
                                                     </td>	
                                                     <td><a href="<?php echo $row->pp_url; ?>" download="" title="<?php echo $row->pp_filename; ?>" ><i class="fa fa-download"></i></a></td>	
-                                                    <td><?php echo date('F d, Y', strtotime($row->pp_dos)); ?></td>	
+                                                    <td><?php  echo date('F d, Y', strtotime($row->pp_dos)); ?></td>	
 
                                                     <td class="menu-action">
                                                         <a href="#" onclick="showAjaxModal('<?php echo base_url(); ?>index.php?modal/popup/modal_edit_participate/<?php echo $row->pp_id; ?>');" data-original-title="edit" data-toggle="tooltip" data-placement="top" class="btn menu-icon vd_bd-yellow vd_yellow"><i class="fa fa-pencil"></i></a>
