a67c8ebdbe3a80256755c0a4eebb83c4a1bdc77c
mayur <mayur@localhost>
2016-04-01 11:31:07 +0530

    Change the button label `Add Course` to `Add Event`

--- application/views/backend/admin/events.php
+++ application/views/backend/admin/events.php
@@ -99,7 +99,7 @@
                                         </div>								
                                         <div class="form-group">
                                             <div class="col-sm-offset-3 col-sm-5">
-                                                <button type="submit" class="btn btn-info">Add Event</button>
+                                                <button type="submit" class="btn btn-info">Add event</button>
                                             </div>
                                         </div>
                                     </div>    
