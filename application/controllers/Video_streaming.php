a6567a5a16a1d9bc109bdbe5c64cc53bc985214b
Mayur Ghadiya <mayur.ghadiya@searchnative.in>
2016-04-05 11:07:24 +0530

    video streaming label changes and validation in sub admin

--- application/controllers/Video_streaming.php
+++ application/controllers/Video_streaming.php
@@ -38,10 +38,12 @@ class Video_streaming extends CI_Controller {
         } else {
             $is_active = 0;
         }
-        
+
         $this->db->insert('broadcast_and_streaming', array(
             'title' => $_POST['title'],
+            'degree_id' => $_POST['degree'],
             'course' => $_POST['course'],
+            'batch' => $_POST['batch'],
             'semester' => $_POST['semester'],
             'url_link' => $url,
             'is_active' => $is_active
@@ -51,17 +53,44 @@ class Video_streaming extends CI_Controller {
     function assign_live_stream() {
         $this->db->where('title', $_POST['title']);
         $this->db->update('broadcast_and_streaming', array(
-            'course'    => $_POST['course'],
-            'semester'  => $_POST['semester'],
+            'course' => $_POST['course'],
+            'semester' => $_POST['semester'],
             'is_active' => $_POST['is_active']
         ));
     }
-    
+
     function in_active_streaming($id) {
         $this->db->where('title', $id);
         $this->db->update('broadcast_and_streaming', array(
             'is_active' => '0'
         ));
+    }
+
+    /**
+     * Course list from degree
+     * @param int $degree_id
+     */
+    function course_list_from_degree($degree_id) {
+        $this->load->model('admin/Crud_model');
+        $course = $this->Crud_model->course_list_from_degree($degree_id);
+
+        echo json_encode($course);
+    }
+
+    /**
+     * Batch list from degree and course
+     * @param int $degree
+     * @param int $course
+     */
+    function batch_list_from_degree_and_course($degree = '', $course = '') {
+        $this->load->model('admin/Crud_model');
+        $batch = $this->Crud_model->batch_list_from_degree_and_course($degree, $course);
+
+        echo json_encode($batch);
+    }
+
+    function student_list_from_degree_course_batch($degree, $course, $batch) {
+        $this->load->model('admin/Crud_model');
     }
 
 }
