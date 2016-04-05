4e6844adc8e9e3de604aaafc891db8c029519505
mayur <mayur@localhost>
2016-04-05 14:06:55 +0530

    search filter

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3844,4 +3844,32 @@ class Admin extends CI_Controller {
         
           
     }
+    function getactivity()
+    {
+        
+         $degree = $this->input->post('degree');
+            $course = $this->input->post('course');
+            $batch = $this->input->post('batch');
+            $semester = $this->input->post("semester");
+            $data['course'] = $this->db->get('course')->result();
+            $data['semester'] = $this->db->get('semester')->result();
+            $data['batch'] = $this->db->get('batch')->result();
+            $data['degree'] = $this->db->get('degree')->result();
+            $data['student'] = $this->db->get('student')->result();
+            
+        $this->db->select("sl.*,s.* ");
+        $this->db->from('participate_student sl');        
+        $this->db->join("student s", "s.std_id=sl.student_id");
+        $this->db->where("s.course_id",$course);
+            $this->db->where("s.std_batch",$batch);
+            $this->db->where("s.std_degree",$degree);
+            $this->db->where("s.semester_id",$semester);
+              $data['volunteer'] = $this->db->get()->result_array();
+
+           $this->load->view("backend/admin/getactivity",$data);
+            
+        
+        
+          
+    }
 }
