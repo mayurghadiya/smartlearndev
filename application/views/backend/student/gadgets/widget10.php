f2a26013f7fb00adba567771e0e9e94791ceee28
mayur <mayur@localhost>
2016-04-06 17:28:08 +0530

    notification

--- application/views/backend/student/gadgets/widget10.php
+++ application/views/backend/student/gadgets/widget10.php
@@ -8,7 +8,12 @@
 					  <ul class="list-wrapper pd-lr-10">
                                               <?php  ?>
                                               
-                                              <li><a href="<?php echo base_url();?>index.php?student/volunteer" target="_blank">Volunteer</a></li>                                            
+                                              <li><a href="<?php echo base_url();?>index.php?student/volunteer" target="_blank">Volunteer
+                                                  <?php
+                            if(isset($this->session->userdata('notifications')['participate_manager'])){ ?>
+                            <img style="margin-top: 5px;" src="<?php echo base_url('assets/images/new_icon.gif'); ?>"/>
+                            <?php } ?>
+                                                  </a></li>                                            
                                               <li>  <a href="<?php echo base_url();?>index.php?student/participate" target="_blank"> <div class="menu-icon"><i class=" fa fa-user"></i></div> <div class="menu-text">Survey</div> </a> </li>
                                               <li>  <a href="<?php echo base_url();?>index.php?student/uploads" target="_blank"> <div class="menu-icon"><i class=" fa fa-user"></i></div> <div class="menu-text">Upload</div> </a> </li>
                                             	
