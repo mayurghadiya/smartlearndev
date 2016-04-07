1afcfd51cc27832840d760ae4a063b209591ce7e
mayur <mayur@localhost>
2016-04-07 16:15:16 +0530

    search

--- application/views/backend/admin/project.php
+++ application/views/backend/admin/project.php
@@ -459,6 +459,8 @@ foreach ($datadegree as $rowdegree) {
            var course =  $("#sub_branches").val();
            var batch =  $("#sub_batches").val();
             var semester = $("#sub_semesters").val();
+            if($("#sub_courses").val()!="" & $("#sub_branches").val()!="" & $("#sub_batches").val()!="" & $("#sub_semesters").val()!="")
+            {
             $.ajax({
                 type:"POST",
                 url:"<?php echo base_url(); ?>index.php?admin/getprojects/submitted",
@@ -470,6 +472,7 @@ foreach ($datadegree as $rowdegree) {
                 
                 
             });
+            }
              return false;
             
             
@@ -479,6 +482,8 @@ foreach ($datadegree as $rowdegree) {
            var course =  $("#branches").val();
            var batch =  $("#batches").val();
             var semester = $("#semesters").val();
+             if($("#courses").val()!="" & $("#branches").val()!="" & $("#batches").val()!="" & $("#semesters").val()!="")
+            {
             $.ajax({
                 type:"POST",
                 url:"<?php echo base_url(); ?>index.php?admin/getprojects/allproject",
@@ -490,6 +495,7 @@ foreach ($datadegree as $rowdegree) {
                 
                 
             });
+            }
              return false;
          });
          $("#courses").change(function(){
