22522455832fc41c4e2c32cf2a7546c50674667d
mayur <mayur@localhost>
2016-04-08 15:26:41 +0530

    btn class

--- application/views/backend/admin/studyresource.php
+++ application/views/backend/admin/studyresource.php
@@ -85,7 +85,7 @@
                                     <div class="form-group col-sm-2">
                                         <label>&nbsp;</label>
                                         
-                                    <button type="submit" class="submit btn btn-info">Search</button>
+                                    <button type="submit" class="submit btn btn-info vd_bg-green">Search</button>
                                     </div>
                                 </div>
                                     </form>
@@ -303,7 +303,7 @@
 
                                         <div class="form-group">
                                             <div class="col-sm-offset-3 col-sm-5">
-                                                <button type="submit" class="btn btn-info">Add Study Resource</button>
+                                                <button type="submit" class="btn btn-info vd_bg-green">Add Study Resource</button>
                                             </div>
                                         </div>
                                         </form>               
