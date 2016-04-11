b933519203383ca2935127e9a54e28bfb4af66f3
Nikita Patel <nikita.patel@searchnative.in>
2016-04-11 15:07:09 +0530

    comment submitted assignment link

--- application/views/backend/student/gadgets/widget3.php
+++ application/views/backend/student/gadgets/widget3.php
@@ -5,14 +5,14 @@
             <div class="col-sm-6 ui-sortable">
                 <div class="content-list content-menu">
                     <ul class="list-wrapper pd-lr-10">
-                     <li><a href="<?php echo base_url(); ?>index.php?student/assignment/assignment_list" target="_blank"> <div class="menu-icon"><i class=" icon-lock"></i></div> <div class="menu-text">Assignments List
+                     <li><a href="<?php echo base_url(); ?>index.php?student/assignment" target="_blank"> <div class="menu-icon"><i class=" icon-lock"></i></div> <div class="menu-text">Assignments List
                              <?php
                             if(isset($this->session->userdata('notifications')['assignment_manager'])){ ?>
                             <img style="margin-top: 5px;" src="<?php echo base_url('assets/images/new_icon.gif'); ?>"/>
                             <?php } ?>
                              </div> </a> </li>
-                      <li> <a href="<?php echo base_url(); ?>index.php?student/assignment/submited_assignment" target="_blank"> <div class="menu-icon"><i class=" fa fa-user"></i></div> <div class="menu-text">Submitted Assignment List</div> </a> </li>
-                 <!--   <li> <a href="<?php echo base_url(); ?>index.php?student/assignment/offline" target<!--="_blank"> <d<!--iv class="menu-icon"><i class=" icon-trophy"></i></div> <div class="menu-text">Offline</div> </a> </li>-->
+                       <!--     <li> <a href="<?php echo base_url(); ?>index.php?student/assignment" target="_blank"> <div class="menu-icon"><i class=" fa fa-user"></i></div> <div class="menu-text">Submitted Assignment List</div> </a> </li>
+              <li> <a href="<?php echo base_url(); ?>index.php?student/assignment/offline" target<!--="_blank"> <d<!--iv class="menu-icon"><i class=" icon-trophy"></i></div> <div class="menu-text">Offline</div> </a> </li>-->
                    
                     </ul>
                 </div>
