39feee7e5f6e8e753d7e9a91d759afb4925cae74
Nikita Patel <nikita.patel@searchnative.in>
2016-04-12 15:01:00 +0530

    create page for message to send email

---
+++ application/views/backend/admin/emailmessage.php
@@ -0,0 +1,6 @@
+<body>
+    Welcome to Search Native India Your login credential is mention below
+    <p>Your user name is <?php echo $email; ?></p>
+    <p>Password is 1234</p>
+    And rollno is <p style='color:#15c;'><?php echo $rollno?></p>
+</body>
