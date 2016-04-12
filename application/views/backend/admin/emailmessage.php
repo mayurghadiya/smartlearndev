d475ee8a0da88274c3c62743725dd6fcca3c749f
Nikita Patel <nikita.patel@searchnative.in>
2016-04-12 15:23:56 +0530

    formate changes

--- application/views/backend/admin/emailmessage.php
+++ application/views/backend/admin/emailmessage.php
@@ -1,6 +1,7 @@
 <body>
-    Welcome to Search Native India Your login credential is mention below
-    <p>Your user name is <?php echo $email; ?></p>
-    <p>Password is 1234</p>
-    And rollno is <p style='color:#15c;'><?php echo $rollno?></p>
+    Welcome to Search Native India, 
+    Your login credential is mention below
+    <p>Your user name is <?php echo $email; ?>
+    <p>Password is <span style='color:#15c;'> 1234</span>
+    <p>And rollno is <span style='color:#15c;'><?php echo $rollno?></span>
 </body>
