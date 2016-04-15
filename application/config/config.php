4f0cc32738afc8f56ffa29bcb13418764d72990f
Mayur Ghadiya <mayur.ghadiya@searchnative.in>
2016-04-15 19:08:31 +0530

    config file dynamic url conflict solved

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/smartlearndev/';
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+$config['base_url']    = $root; 
 //$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'';  
 $config['image_path']= $config['base_url']."/assets/img/";
 $config['upload_img_path']= $config['base_url']."/uploads/";
