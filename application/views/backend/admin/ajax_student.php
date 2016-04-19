c1280b304506964be1ddbba39a40a050e0e1af8e
Nikita Patel <nikita.patel@searchnative.in>
2016-04-19 17:00:35 +0530

    remove photo td

--- application/views/backend/admin/ajax_student.php
+++ application/views/backend/admin/ajax_student.php
@@ -20,8 +20,7 @@ $.toaster({
 <thead>
     <tr>
         <th><div>#</div></th>												
-        <th><div>Name</div></th>												
-        <th><div></div></th>												
+        <th><div>Name</div></th>											
         <th><div>Full Name</div></th>												
         <th><div>Email</div></th>												
         <th><div>Mobile No</div></th>												
@@ -36,7 +35,7 @@ $.toaster({
         <tr>
             <td><?php echo $count++; ?></td>											
             <td><?php echo $row->name; ?></td>											
-            <td>
+           <!-- <td>
                 <?php
                 if($row->profile_photo != '') { ?>
                     <img src="<?= base_url() ?>uploads/student_image/<?= $row->profile_photo; ?>" height="70px" width="70px"/>
@@ -48,7 +47,7 @@ $.toaster({
                     <?php }
                 }
                 ?>
-             </td>											
+             </td>-->											
             <td><?php echo $row->std_first_name . " " . $row->std_last_name; ?></td>					
             <td><?php echo $row->email; ?></td>											
             <td><?php echo $row->std_mobile; ?></td>											
