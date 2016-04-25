c4f491d5b68786bb126405548a96c28b48247a4d
Nikita Patel <nikita.patel@searchnative.in>
2016-04-25 16:16:17 +0530

    set UCWORD for label first letter capital for each word

--- application/views/backend/admin/cms.php
+++ application/views/backend/admin/cms.php
@@ -114,7 +114,7 @@
                                         </div>
                                         <div class="form-group">
                                             <div class="col-sm-offset-3 col-sm-5">
-                                                <button type="submit" class="btn btn-info vd_bg-green"><?php echo ucwords("Add CMS");?></button>
+                                                <button type="submit" class="btn btn-info vd_bg-green"><?php echo ucwords("Add");?></button>
                                             </div>
                                         </div>
                                         </form>               
--- application/views/backend/admin/remedial_exam_time_table.php
+++ application/views/backend/admin/remedial_exam_time_table.php
@@ -176,7 +176,7 @@
                                         </div>
                                         <div class="form-group">
                                             <div class="col-sm-offset-3 col-sm-5">
-                                                <button type="submit" class="btn btn-info vd_bg-green"><?php echo ucwords("Add Exam Schedule");?></button>
+                                                <button type="submit" class="btn btn-info vd_bg-green"><?php echo ucwords("Add");?></button>
                                             </div>
                                         </div>
                                         </form>               
