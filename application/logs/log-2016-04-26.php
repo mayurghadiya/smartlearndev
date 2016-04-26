c2f4daff65dd8afa7f086475daee283504efd188
mayur <mayur@localhost>
2016-04-26 18:15:49 +0530

    mkdir

---
+++ application/logs/log-2016-04-26.php
@@ -0,0 +1,36 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-04-26 12:57:00 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/syllabus.php 81
+ERROR - 2016-04-26 12:58:42 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/syllabus.php 81
+ERROR - 2016-04-26 13:47:35 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/syllabus.php 81
+ERROR - 2016-04-26 13:48:11 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/syllabus.php 81
+ERROR - 2016-04-26 13:54:56 --> Severity: Notice --> Undefined property: stdClass::$assign_id /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/syllabus.php 118
+ERROR - 2016-04-26 13:54:56 --> Severity: Notice --> Undefined property: stdClass::$assign_id /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/syllabus.php 120
+ERROR - 2016-04-26 14:32:59 --> 404 Page Not Found: Syllabus/index
+ERROR - 2016-04-26 14:33:16 --> 404 Page Not Found: Syllabus/index
+ERROR - 2016-04-26 14:33:57 --> 404 Page Not Found: Course/index
+ERROR - 2016-04-26 14:35:23 --> 404 Page Not Found: Syllabus/index
+ERROR - 2016-04-26 14:59:19 --> Severity: Notice --> Undefined property: stdClass::$stllabus_desc /opt/lampp/htdocs/smartlearndev/application/views/site/syllabus.php 32
+ERROR - 2016-04-26 14:59:19 --> Severity: Notice --> Undefined property: stdClass::$stllabus_desc /opt/lampp/htdocs/smartlearndev/application/views/site/syllabus.php 32
+ERROR - 2016-04-26 14:59:19 --> Severity: Notice --> Undefined property: stdClass::$stllabus_desc /opt/lampp/htdocs/smartlearndev/application/views/site/syllabus.php 32
+ERROR - 2016-04-26 15:00:34 --> Severity: error --> Exception: Cannot use object of type CI_DB_mysqli_result as array /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 189
+ERROR - 2016-04-26 15:00:34 --> Severity: Error --> Uncaught TypeError: Argument 1 passed to CI_Exceptions::show_exception() must be an instance of Exception, instance of Error given, called in /opt/lampp/htdocs/smartlearndev/system/core/Common.php on line 658 and defined in /opt/lampp/htdocs/smartlearndev/system/core/Exceptions.php:190
+Stack trace:
+#0 /opt/lampp/htdocs/smartlearndev/system/core/Common.php(658): CI_Exceptions->show_exception(Object(Error))
+#1 [internal function]: _exception_handler(Object(Error))
+#2 {main}
+  thrown /opt/lampp/htdocs/smartlearndev/system/core/Exceptions.php 190
+ERROR - 2016-04-26 15:02:24 --> Severity: error --> Exception: Cannot use object of type CI_DB_mysqli_result as array /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 189
+ERROR - 2016-04-26 15:02:24 --> Severity: Error --> Uncaught TypeError: Argument 1 passed to CI_Exceptions::show_exception() must be an instance of Exception, instance of Error given, called in /opt/lampp/htdocs/smartlearndev/system/core/Common.php on line 658 and defined in /opt/lampp/htdocs/smartlearndev/system/core/Exceptions.php:190
+Stack trace:
+#0 /opt/lampp/htdocs/smartlearndev/system/core/Common.php(658): CI_Exceptions->show_exception(Object(Error))
+#1 [internal function]: _exception_handler(Object(Error))
+#2 {main}
+  thrown /opt/lampp/htdocs/smartlearndev/system/core/Exceptions.php 190
+ERROR - 2016-04-26 15:02:50 --> Severity: error --> Exception: Cannot use object of type CI_DB_mysqli_result as array /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 189
+ERROR - 2016-04-26 15:02:50 --> Severity: Error --> Uncaught TypeError: Argument 1 passed to CI_Exceptions::show_exception() must be an instance of Exception, instance of Error given, called in /opt/lampp/htdocs/smartlearndev/system/core/Common.php on line 658 and defined in /opt/lampp/htdocs/smartlearndev/system/core/Exceptions.php:190
+Stack trace:
+#0 /opt/lampp/htdocs/smartlearndev/system/core/Common.php(658): CI_Exceptions->show_exception(Object(Error))
+#1 [internal function]: _exception_handler(Object(Error))
+#2 {main}
+  thrown /opt/lampp/htdocs/smartlearndev/system/core/Exceptions.php 190
