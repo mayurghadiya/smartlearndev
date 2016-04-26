6b243c3429617a5a13648f720393206d35543c0f
Mayur Ghadiya <mayur.ghadiya@searchnative.in>
2016-04-26 18:38:38 +0530

    edit graduate validation

---
+++ application/logs/log-2016-04-26.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-04-26 18:34:45 --> Severity: Notice --> Undefined index: year /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 5263
