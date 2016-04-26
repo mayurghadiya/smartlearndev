9f6e5a95d57cba15944e149f15052bcf6e25a0b8
mayur <mayur@localhost>
2016-04-26 18:49:32 +0530

    unstage

---
+++ application/views/backend/admin/graduate.php
@@ -0,0 +1,285 @@
+
+<!-- Middle Content Start -->    
+<div class="vd_content-wrapper">
+    <div class="vd_container">
+        <div class="vd_content clearfix">
+            <div class="vd_head-section clearfix">
+                <div class="vd_panel-header">
+                    <ul class="breadcrumb">
+                        <li><a href="#"><?php echo ucwords("home"); ?></a> </li>
+                        <li><a href="#"><?php echo ucwords("pages"); ?></a> </li>
+                        <li class="active"><?php echo ucwords("recent graduates"); ?></li>
+                    </ul>                  
+                </div>
+            </div>
+            <div class="vd_title-section clearfix">
+                <div class="vd_panel-header no-subtitle">
+                    <h1><?php echo ucwords("recent graduates"); ?></h1>
+                </div>
+            </div>
+            <div class="vd_content-section clearfix">
+                <div class="row">
+                    <div class="col-sm-12">								
+                        <!------CONTROL TABS START------>
+                        <ul class="nav nav-tabs bordered">
+                            <li class="active">
+                                <a href="#list" data-toggle="tab"><i class="entypo-menu"></i> 
+                                    <?php echo ucwords("graduates list"); ?>
+                                </a></li>
+                            <li>
+                                <a href="#add" data-toggle="tab"><i class="entypo-plus-circled"></i>
+                                    <?php echo ucwords("add graduates"); ?> 
+                                </a></li>
+                        </ul>
+                        <!------CONTROL TABS END------>
+
+                        <div class="tab-content">
+                            <!----TABLE LISTING STARTS-->
+                            <div class="tab-pane box active" id="list">
+
+                                <div class="panel-body table-responsive">
+                                    <table class="table table-striped table-responsive" id="data-tables">
+                                        <thead>
+                                            <tr>
+                                                <th><div>#</div></th>
+                                                <th><?php echo ucwords("student name"); ?></th>
+                                                <th><?php echo ucwords("course"); ?></th>
+                                                <th><?php echo ucwords("branch"); ?></th>
+                                                <th><?php echo ucwords("year"); ?></th>
+                                                <th><?php echo ucwords("action"); ?></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php $counter = 1; ?>
+                                            <?php foreach ($graduates as $row) { ?>
+                                                <tr>
+                                                    <td><?php echo $counter++; ?></td>
+                                                    <td><?php echo $row->std_first_name . ' ' . $row->std_last_name; ?></td>
+                                                    <td><?php echo $row->d_name; ?></td>
+                                                    <td><?php echo $row->c_name; ?></td>
+                                                    <td><?php echo $row->graduate_year; ?></td>
+                                                    <td>
+                                                        <a href="#" onclick="showAjaxModal('<?php echo base_url(); ?>index.php?modal/popup/modal_edit_graduate/<?php echo $row->graduates_id; ?>');" data-original-title="edit" data-toggle="tooltip" data-placement="top" class="btn menu-icon vd_bd-yellow vd_yellow"><i class="fa fa-pencil"></i></a>
+
+                                                        <a href="#" onclick="confirm_modal('<?php echo base_url(); ?>index.php?admin/graduate/delete/<?php echo $row->graduates_id; ?>');" data-original-title="remove" data-toggle="tooltip" data-placement="top" class="btn menu-icon vd_bd-red vd_red"><i class="fa fa-times"></i> </a>
+                                                    
+                                                    </td>
+                                                </tr>	
+                                            <?php } ?>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <!----TABLE LISTING ENDS--->
+
+
+                            <!----CREATION FORM STARTS---->
+                            <div class="tab-pane box" id="add" style="padding: 5px">
+                                <div class="box-content">     
+                                    <div class="">
+                                        <span style="color:red">* <?php echo ucwords("is mandatory field"); ?></span> 
+                                    </div>                                    
+                                    <?php echo form_open(base_url() . 'index.php?admin/graduate/create', array('class' => 'form-horizontal form-groups-bordered validate', 'role' => 'form', 'id' => 'graduatesform', 'enctype' => 'multipart/form-data', 'target' => '_top')); ?>
+                                    <div class="padded">
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label"><?php echo ucwords("course name"); ?><span style="color:red">*</span></label>
+                                            <div class="col-sm-5">
+                                                <select class="form-control" id="degree" name="degree">
+                                                    <option value="">Select</option>
+                                                    <?php foreach ($degree as $row) { ?>
+                                                        <option value="<?php echo $row->d_id; ?>"><?php echo $row->d_name; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+                                        </div>	
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label"><?php echo ucwords("Branch"); ?><span style="color:red">*</span></label>
+                                            <div class="col-sm-5">
+                                                <select class="form-control" name="course" id="course">
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label"><?php echo ucwords("Batch"); ?><span style="color:red">*</span></label>
+                                            <div class="col-sm-5">
+                                                <select class="form-control" name="batch" id="batch">
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label"><?php echo ucwords("Semester"); ?><span style="color:red">*</span></label>
+                                            <div class="col-sm-5">
+                                                <select class="form-control" name="semester" id="semester">
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label"><?php echo ucwords("Student"); ?><span style="color:red">*</span></label>
+                                            <div class="col-sm-5">
+                                                <select class="form-control" name="student" id="student">
+
+                                                </select>
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label"><?php echo ucwords("graduate year"); ?><span style="color:red">*</span></label>
+                                            <div class="col-sm-5">
+                                                <input type="text" name="year" id="year" class="form-control"/>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label"><?php echo ucwords("description"); ?></label>
+                                            <div class="col-sm-5">
+                                                <textarea class="form-control" name="description" id="description"></textarea>
+                                                
+                                            </div>
+                                        </div>
+
+
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-3 col-sm-5">
+                                                <button type="submit" class="btn btn-info vd_bg-green" ><?php echo ucwords("add"); ?></button>
+                                            </div>
+                                        </div>
+                                        </form>               
+                                    </div>                
+                                </div>
+                                <!----CREATION FORM ENDS-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>              
+        </div>
+        <!-- row --> 
+    </div>
+    <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.js"></script>
+    <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.validate.min.js"></script>
+    <script type="text/javascript">
+        $.validator.setDefaults({
+            submitHandler: function (form) {
+                form.submit();
+            }
+        });
+
+
+        $().ready(function () {
+
+            $("#graduatesform").validate({
+                rules: {
+                    degree: "required",
+                    course: "required",
+                    batch: "required",
+                    semester: "required",
+                    student: "required",
+                    year: "required"
+                },
+                messages: {
+                    degree: "Please select course",
+                    course: "Please select branch",
+                    batch: "Please select batch",
+                    semester: "Please select semester",
+                    student: "Please select student",
+                    year: "Please enter year"
+                }
+            });
+        });
+    </script>
+
+    <script>
+        $(document).ready(function () {
+            //course by degree
+            $('#degree').on('change', function () {
+                var course_id = $('#course').val();
+                var degree_id = $(this).val();
+                //remove all present element
+                $('#course').find('option').remove().end();
+                $('#course').append('<option value="">Select</option>');
+                var degree_id = $(this).val();
+                $.ajax({
+                    url: '<?php echo base_url(); ?>index.php?admin/course_list_from_degree/' + degree_id,
+                    type: 'get',
+                    success: function (content) {
+                        var course = jQuery.parseJSON(content);
+                        $.each(course, function (key, value) {
+                            $('#course').append('<option value=' + value.course_id + '>' + value.c_name + '</option>');
+                        })
+                    }
+                })
+                batch_from_degree_and_course(degree_id, course_id);
+            });
+            //batch from course and degree
+            $('#course').on('change', function () {
+                var degree_id = $('#degree').val();
+                var course_id = $(this).val();
+                batch_from_degree_and_course(degree_id, course_id);
+                get_semester_from_branch(course_id);
+            });
+
+            $('#semester').on('change', function () {
+                var degree = $('#degree').val();
+                var course = $('#course').val();
+                var batch = $('#batch').val();
+                var semester = $('#semester').val();
+                student_list_from_degree_course_batch_semester(degree, course, batch, semester);
+            });
+
+            //find batch from degree and course
+            function batch_from_degree_and_course(degree_id, course_id) {
+                //remove all element from batch
+                $('#batch').find('option').remove().end();
+                $.ajax({
+                    url: '<?php echo base_url(); ?>index.php?admin/batch_list_from_degree_and_course/' + degree_id + '/' + course_id,
+                    type: 'get',
+                    success: function (content) {
+                        $('#batch').append('<option value="">Select</option>');
+                        var batch = jQuery.parseJSON(content);
+                        console.log(batch);
+                        $.each(batch, function (key, value) {
+                            $('#batch').append('<option value=' + value.b_id + '>' + value.b_name + '</option>');
+                        })
+                    }
+                })
+            }
+
+            //get semester from brach
+            function get_semester_from_branch(branch_id) {
+                $('#semester').find('option').remove().end();
+                $.ajax({
+                    url: '<?php echo base_url(); ?>index.php?admin/get_semesters_of_branch/' + branch_id,
+                    type: 'get',
+                    success: function (content) {
+                        $('#semester').append('<option value="">Select</option>');
+                        var semester = jQuery.parseJSON(content);
+                        $.each(semester, function (key, value) {
+                            $('#semester').append('<option value=' + value.s_id + '>' + value.s_name + '</option>');
+                        })
+                    }
+                })
+            }
+
+            //student list from degree, course, batch and semester
+            function student_list_from_degree_course_batch_semester(degree, courese, batch, semester) {
+                $('#student').find('option').remove().end();
+                $.ajax({
+                    url: '<?php echo base_url(); ?>index.php?admin/student_list_from_degree_course_batch_semester/' + degree + '/'
+                            + courese + '/' + batch + '/' + semester,
+                    type: 'get',
+                    success: function (content) {
+                        $('#student').append('<option value="">Select</option>');
+                        var student = jQuery.parseJSON(content);
+                        $.each(student, function (key, value) {
+                            $('#student').append('<option value=' + value.std_id + '>' + value.std_first_name + ' ' + value.std_last_name + '</option>');
+                        });
+                    }
+                });
+            }
+
+        })
+    </script>
