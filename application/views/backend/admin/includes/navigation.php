bdebdd06a9935c07854f68393b1ad85674aad48d
Nikita Patel <nikita.patel@searchnative.in>
2016-04-29 16:20:18 +0530

    put condition for holiday and chancellor

--- application/views/backend/admin/includes/navigation.php
+++ application/views/backend/admin/includes/navigation.php
@@ -11,12 +11,12 @@
             <ul>                  
 
                 <li>
-                    <a href="javascript:void(0);" data-action="click-trigger" <?php if ($page_name == "degree" || $page_name == "course" || $page_name == "batch" || $page_name == "semesterlist" || $page_name == "admission_type" || $page_name == "student" || $page_name == "subject") { ?> class="open" <?php } ?> >
+                    <a href="javascript:void(0);" data-action="click-trigger" <?php if ($page_name == "degree" || $page_name == "course" || $page_name == "batch" || $page_name == "semesterlist" || $page_name == "admission_type" || $page_name == "student" || $page_name == "subject" || $page_name == "holiday" || $page_name == "chancellor" ) { ?> class="open" <?php } ?> >
                         <span class="menu-icon entypo-icon"><i class="icon-tools"></i></span> 
                         <span class="menu-text">Basic Management</span>  
                         <span class="menu-badge"><span class="badge vd_bg-black-30"><i class="fa fa-angle-down"></i></span></span>
                     </a>
-                    <div class="child-menu"  data-action="click-target" <?php if ($page_name == "degree" || $page_name == "course" || $page_name == "batch" || $page_name == "semesterlist" || $page_name == "admission_type" || $page_name == "student" || $page_name == "subject" || $page_name=="syllabus") { ?> style="display: block" <?php } ?>>
+                    <div class="child-menu"  data-action="click-target" <?php if ($page_name == "degree" || $page_name == "course" || $page_name == "batch" || $page_name == "semesterlist" || $page_name == "admission_type" || $page_name == "student" || $page_name == "subject" || $page_name=="syllabus" || $page_name == "holiday" || $page_name == "chancellor" ) { ?> style="display: block" <?php } ?>>
                         <ul>
                             <li <?php if ($page_name == "degree") { ?> class="selectednavmenu" <?php } ?>>
                                 <a href="<?php echo base_url(); ?>index.php?admin/degree"> 
