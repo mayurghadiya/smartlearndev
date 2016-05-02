5ea10def14c37cbed0f95810dc629c403258a978
mayur <mayur@localhost>
2016-05-02 17:32:35 +0530

    sorting arrow

---
+++ application/logs/log-2016-05-02.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-02 17:25:59 --> Severity: Notice --> Undefined variable: widget_order /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/includes_top.php 120
+ERROR - 2016-05-02 17:25:59 --> Severity: Notice --> Undefined variable: widget_order /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/includes_top.php 149
+ERROR - 2016-05-02 17:25:59 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-02 17:26:05 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-02 17:26:05 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:26:05 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-02 17:26:05 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-02 17:26:24 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:26:24 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-02 17:26:24 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-02 17:26:42 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:26:50 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:28:07 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:28:20 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:28:53 --> Severity: Notice --> Undefined variable: widget_order /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/includes_top.php 120
+ERROR - 2016-05-02 17:28:53 --> Severity: Notice --> Undefined variable: widget_order /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/includes_top.php 149
+ERROR - 2016-05-02 17:28:53 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-02 17:28:54 --> 404 Page Not Found: Img/logo.png
