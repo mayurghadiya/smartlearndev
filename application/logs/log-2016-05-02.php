4f8b682117ab064a5d595d4993a28be9ea654a81
mayur <mayur@localhost>
2016-05-02 17:57:40 +0530

    media index redirect on photo gallery

--- application/logs/log-2016-05-02.php
+++ application/logs/log-2016-05-02.php
@@ -18,3 +18,69 @@ ERROR - 2016-05-02 17:28:53 --> Severity: Notice --> Undefined variable: widget_
 ERROR - 2016-05-02 17:28:53 --> Severity: Notice --> Undefined variable: widget_order /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/includes_top.php 149
 ERROR - 2016-05-02 17:28:53 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
 ERROR - 2016-05-02 17:28:54 --> 404 Page Not Found: Img/logo.png
+ERROR - 2016-05-02 17:33:18 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:33:40 --> 404 Page Not Found: Assets/images
+ERROR - 2016-05-02 17:33:55 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:34:14 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-02 17:34:14 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:34:14 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-02 17:34:14 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-02 17:35:07 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:35:08 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:36:18 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:21 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:21 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:22 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:22 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:23 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:47:24 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:24 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:24 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:25 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:47:26 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:48:15 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-02 17:48:48 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:49:58 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:51:19 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:52:14 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:52:59 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:53:02 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:53:04 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:53:06 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:53:47 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:53:52 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:54:01 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:54:06 --> 404 Page Not Found: Media/img
+ERROR - 2016-05-02 17:54:07 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-02 17:54:11 --> Severity: Notice --> Undefined variable: gallery /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/photo_gallery.php 104
+ERROR - 2016-05-02 17:54:11 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/photo_gallery.php 104
+ERROR - 2016-05-02 17:54:13 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:54:13 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-02 17:55:24 --> 404 Page Not Found: Photogallery/index
+ERROR - 2016-05-02 17:55:41 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-02 17:55:45 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:55:45 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-02 17:55:45 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-02 17:55:49 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:55:49 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-02 17:55:49 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-02 17:55:55 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-02 17:55:59 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:55:59 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-02 17:55:59 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-02 17:56:05 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:56:09 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-02 17:56:09 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-02 17:56:09 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-02 17:56:11 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-02 17:56:12 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:56:15 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:56:16 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-02 17:56:40 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:56:51 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:56:54 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:56:59 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:57:02 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:57:05 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:57:08 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-02 17:57:10 --> 404 Page Not Found: Admin/mainfest.appcache
