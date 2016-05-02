2e92d629ca907500254c784add33ba0fb59f46d5
Nikita Patel <nikita.patel@searchnative.in>
2016-05-02 17:07:31 +0530

    create function to check project at update time

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4783,6 +4783,18 @@ class Admin extends CI_Controller {
                     'pm_batch' => $batch, 'pm_semester' => $semester))->result_array();
         echo json_encode($data);
     }
+    function checkprject($id = '') {
+        $degree = $this->input->post('degree');
+        $course = $this->input->post('course');
+        $batch = $this->input->post('batch');
+        $semester = $this->input->post('semester');
+        $title = $this->input->post('title');
+        $data = $this->db->get_where('project_manager', array('pm_degree' => $degree,
+                    'pm_course' => $course,
+                    'pm_title' => $title,
+                    'pm_batch' => $batch, 'pm_semester' => $semester))->result_array();
+        echo json_encode($data);
+    }
 
     function checkassignments() {
         $degree = $this->input->post('degree');
@@ -4793,7 +4805,7 @@ class Admin extends CI_Controller {
         $data = $this->db->get_where('assignment_manager', array('assign_degree' => $degree,
                     'course_id' => $course,
                     'assign_title' => $title,
-                    'assign_batch' => $batch, 'assign_sem' => $semester))->result_array();
+                    'assign_batch' => $batch, 'assign_sem' => $semester,'pm_id!='=>$id))->result_array();
         echo json_encode($data);
     }
     function checkassignment($id = '') {
