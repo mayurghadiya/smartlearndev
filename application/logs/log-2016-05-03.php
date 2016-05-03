11d12f2583f65a77681b8139817486066a86d1c4
mayur <mayur@localhost>
2016-05-03 11:18:59 +0530

    add event in popup

---
+++ application/logs/log-2016-05-03.php
@@ -0,0 +1,50 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-03 10:04:31 --> 404 Page Not Found: Admin/img
+ERROR - 2016-05-03 10:04:39 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 10:12:49 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-03 10:12:49 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-03 10:12:51 --> 404 Page Not Found: Img/ico
+ERROR - 2016-05-03 10:13:02 --> 404 Page Not Found: Img/ico
+ERROR - 2016-05-03 10:13:06 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-03 10:13:06 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-03 10:13:06 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-03 10:13:51 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-03 10:13:51 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-03 10:13:52 --> 404 Page Not Found: Img/ico
+ERROR - 2016-05-03 10:17:59 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 10:17:59 --> 404 Page Not Found: Admin/img
+ERROR - 2016-05-03 10:22:32 --> 404 Page Not Found: Admin/img
+ERROR - 2016-05-03 10:22:36 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 10:43:01 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 10:43:47 --> Query error: Unknown column 'fdfdgfdgfd' in 'where clause' - Invalid query: select * from batch where degree_id in(3) and course_id in(7) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:58 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:58 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:58 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:59 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:59 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:59 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:59 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:43:59 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:44:00 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:44:00 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') and course_id in(default) and b_name=fdfdgfdgfd' at line 1 - Invalid query: select * from batch where degree_id in() and course_id in(default) and b_name=fdfdgfdgfd
+ERROR - 2016-05-03 10:50:56 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 10:50:58 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 10:51:06 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-03 10:51:15 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 10:53:38 --> Severity: Notice --> Trying to get property of non-object /opt/lampp/htdocs/smartlearndev/application/helpers/multi_language_helper.php 32
+ERROR - 2016-05-03 10:58:29 --> Severity: Notice --> Undefined variable: group /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/addevent.php 47
+ERROR - 2016-05-03 10:58:30 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/addevent.php 47
+ERROR - 2016-05-03 11:00:47 --> Severity: Notice --> Undefined variable: group /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/addevent.php 47
+ERROR - 2016-05-03 11:00:47 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/addevent.php 47
+ERROR - 2016-05-03 11:00:54 --> Severity: Notice --> Undefined variable: group /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/addevent.php 47
+ERROR - 2016-05-03 11:00:54 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/views/backend/admin/addevent.php 47
+ERROR - 2016-05-03 11:03:06 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 11:03:15 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 11:08:49 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 11:15:04 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 11:16:21 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 11:17:27 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 11:17:53 --> Severity: Warning --> implode(): Invalid arguments passed /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 280
+ERROR - 2016-05-03 11:17:53 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ')' at line 1 - Invalid query: select * from course where degree_id in()
+ERROR - 2016-05-03 11:18:10 --> 404 Page Not Found: Admin/mainfest.appcache
