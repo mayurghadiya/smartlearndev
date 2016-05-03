c7f760988fb6c55df6d30cb26418481fb476c4ec
Nikita Patel <nikita.patel@searchnative.in>
2016-05-03 15:31:04 +0530

    set breadcrumb

--- application/views/backend/admin/restore.php
+++ application/views/backend/admin/restore.php
@@ -4,10 +4,11 @@
         <div class="vd_content clearfix">
             <div class="vd_head-section clearfix">
                 <div class="vd_panel-header">
-                    <ul class="breadcrumb">
-                         <li><?php echo set_breadcrumb(); ?></li>
-                    </ul>
-                    
+                  <ul class="breadcrumb">
+                         <li><a href="<?php echo base_url('index.php?admin/dashboard'); ?>"><?php echo ucwords("home");?></a> </li>
+                         <li><?php echo ucwords("backup/restore");?></li>
+                         <li><?php echo ucwords("restore");?></li>
+                    </ul> 
                 </div>
             </div>
             <div class="vd_title-section clearfix">
