364da958636d60f5f8a6f416e8915398b45caf38
mayur <mayur@localhost>
2016-05-03 18:20:19 +0530

    add modal

--- application/views/backend/admin/charity_fund.php
+++ application/views/backend/admin/charity_fund.php
@@ -63,120 +63,7 @@
 
                             <!----CREATION FORM STARTS---->
                             <div class="tab-pane box" id="add" style="padding: 5px">
-                                <div class="box-content">     
-                                    <div class="">
-                                        <span style="color:red">* <?php echo "is ".ucwords("mandatory field");?></span> 
-                                    </div>                                    
-                                    <?php echo form_open(base_url() . 'index.php?admin/charity_fund/create', array('class' => 'form-horizontal form-groups-bordered validate', 'role' => 'form', 'id' => 'chartiyfund', 'target' => '_top')); ?>
-                                    <div class="padded">
-                                        <div class="form-group">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("donor name"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input type="text" class="form-control" name="donor_name" id="donor_name"/>
-                                            </div>
-                                        </div>												
-                                        <div class="form-group">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("donor mobile"); ?></label>
-                                            <div class="col-sm-5">
-                                                <input type="text" class="form-control" name="donor_mobile"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("donor email"); ?></label>
-                                            <div class="col-sm-5">
-                                                <input type="text" class="form-control" name="donor_email"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("amount"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input type="text" class="form-control" name="amount"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("donation method"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <select class="form-control" name="donation_type" id="donation_type">
-                                                    <option value="">Select</option>
-                                                    <option value="cheque">Cheque</option>
-                                                    <option value="dd">DD</option>
-                                                </select>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group cheque-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("cheque nomber"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control cheque-details-fields" name="cheque_cheque_number"/>
-                                            </div>	
-                                        </div>                                        
-                                        <div class="form-group cheque-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("account number"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control cheque-details-fields" name="cheque_account_number"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group cheque-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("account holder name"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control cheque-details-fields" name="cheque_account_holder_name"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group cheque-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("branch code"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control cheque-details-fields" name="cheque_branch_code"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group cheque-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("bank name"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control cheque-details-fields" name="cheque_bank_name"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group dd-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("account number"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control dd-details-fields" name="dd_account_number"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group dd-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("account holder name"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control dd-details-fields" name="dd_account_holder_name"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group dd-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("branch code"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control dd-details-fields" name="dd_branch_code"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group dd-details hidden">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("bank name"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control dd-details-fields" name="dd_bank_name"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("date"); ?><span style="color:red">*</span></label>
-                                            <div class="col-sm-5">
-                                                <input class="form-control datepicker-normal" readonly="" required="" type="text" name="date"/>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group">
-                                            <label class="col-sm-3 control-label"><?php echo ucwords("description"); ?></label>
-                                            <div class="col-sm-5">
-                                                <textarea class="form-control" name="description" id="description"></textarea>
-                                            </div>	
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="col-sm-offset-3 col-sm-5">
-                                                <button type="submit" class="btn btn-info vd_bg-green" ><?php echo ucwords("add"); ?></button>
-                                            </div>
-                                        </div>
-                                        </form>               
-                                    </div>                
-                                </div>
+                                
                                 <!----CREATION FORM ENDS-->
                             </div>
                         </div>
@@ -188,82 +75,22 @@
     </div>
     <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.js"></script>
     <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.validate.min.js"></script>
-    <script type="text/javascript">
-        $.validator.setDefaults({
-            submitHandler: function (form) {
-                form.submit();
-            }
-        });
+      <style>
+    .nav-fixedtabs {
+    left: 86%;
+    position: fixed;
+    top: 25%;
+    }
+    #navfixed{
+        cursor: pointer;
+    }
+    
+    </style>
+    
+  
+    <div class="md-fab-wrapper">
 
-
-        $().ready(function () {
-
-            $("#chartiyfund").validate({
-                rules: {
-                    donor_name: "required",
-                    amount: "required",
-                    donation_type: "required"
-                },
-                messages: {
-                    donor_name: "Please enter donor name",
-                    amount: "Please enter amount",
-                    donation_type: "Please select donation type"
-                }
-            });
-        });
-    </script>
-
-    <script>
-        $(document).ready(function () {
-            $('#donation_type').on('change', function () {
-                var donation_type = $(this).val();
-                if (donation_type) {
-                    show_hide_information(donation_type);
-                } else {
-                    hide_cheque_details();
-                    hide_dd_details();
-                }
-            });
-
-            function show_hide_information(donation_type) {
-                if (donation_type == 'cheque') {
-                    hide_dd_details();
-                    show_cheque_details();
-                } else if (donation_type == 'dd') {
-                    hide_cheque_details();
-                    show_dd_details();
-
-                }
-            }
-
-            function show_cheque_details() {
-                $('.cheque-details').attr('class', 'form-group cheque-details');
-                $('.cheque-details-fields').attr('required', 'required');
-            }
-
-            function hide_cheque_details() {
-                $('.cheque-details').attr('class', 'form-group cheque-details hidden');
-                $('.cheque-details-fields').removeAttr('required');
-            }
-
-            function show_dd_details() {
-                $('.dd-details').attr('class', 'form-group dd-details');
-                $('.dd-details-fields').attr('required', 'required');
-            }
-
-            function hide_dd_details() {
-                $('.dd-details').attr('class', 'form-group dd-details hidden');
-                $('.dd-details-fields').removeAttr('required');
-            }
-        });
-
-        $(window).load(function () {
-            $('.datepicker-normal').datepicker({
-               dateFormat: 'dd M yy',
-                changeMonth: true,
-                changeYear: true,
-                minDate: new Date(), 
-            });
-        })
-    </script>
-    <?php include('plus_icon.php'); ?>
+        <a class="md-fab md-fab-success nav-fixed-a-tabs vd_bg-red"  onclick="showAjaxModal('<?php echo base_url(); ?>index.php?modal/popup/addcharity/');" href="#" id="navfixed" data-toggle="tab">
+            <i class="material-icons">&#xE145;</i>
+        </a>
+    </div>
