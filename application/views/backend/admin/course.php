cbad824f40bc56ef49a43c60efa23ce4f175b6c6
Nikita Patel <nikita.patel@searchnative.in>
2016-05-03 14:51:58 +0530

    set breadcrumbs of basic management

--- application/views/backend/admin/course.php
+++ application/views/backend/admin/course.php
@@ -6,7 +6,10 @@
             <div class="vd_head-section clearfix">
                 <div class="vd_panel-header">
                     <ul class="breadcrumb">
-                         <li><?php echo set_breadcrumb(); ?></li>
+                        <li><a href="<?php echo base_url('index.php?admin/dashboard'); ?>"><?php echo ucwords("home");?></a> </li>
+                         <li><?php echo ucwords("basic management");?></li>
+                         <li><?php echo ucwords("course");?></li>
+                        
                     </ul>
                   
                 </div>
--- application/views/backend/admin/semesterlist.php
+++ application/views/backend/admin/semesterlist.php
@@ -5,7 +5,9 @@
             <div class="vd_head-section clearfix">
                 <div class="vd_panel-header">
                     <ul class="breadcrumb">
-                        <li><?php echo set_breadcrumb(); ?></li>
+                         <li><a href="<?php echo base_url('index.php?admin/dashboard'); ?>"><?php echo ucwords("home");?></a> </li>
+                         <li><?php echo ucwords("basic management");?></li>
+                         <li><?php echo ucwords("semester");?></li>
                     </ul>
                 </div>
             </div>
