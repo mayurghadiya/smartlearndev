9fcc066d1ef004c27eca16e106bc7e81f7a83514
Nikita Patel <nikita.patel@searchnative.in>
2016-05-03 14:55:53 +0530

    remove and replace page

---
+++ application/views/backend/admin/admission_type.php
@@ -0,0 +1,150 @@
+<!-- Middle Content Start -->    
+<div class="vd_content-wrapper">
+    <div class="vd_container">
+        <div class="vd_content clearfix">
+            <div class="vd_head-section clearfix">
+                <div class="vd_panel-header">
+                   <ul class="breadcrumb">
+                         <li><a href="<?php echo base_url('index.php?admin/dashboard'); ?>"><?php echo ucwords("home");?></a> </li>
+                         <li><?php echo ucwords("basic management");?></li>
+                         <li><?php echo ucwords("admission type");?></li>
+                    </ul>					
+                </div>
+            </div>
+            <div class="vd_title-section clearfix">
+                <div class="vd_panel-header no-subtitle">
+                    <h1>Admission Type Management</h1>
+                </div>
+            </div>
+            <div class="vd_content-section clearfix">
+                <div class="row">
+                    <div class="col-sm-12">								
+                        <!------CONTROL TABS START------>
+                        <ul class="nav nav-tabs bordered">
+                            <li class="active">
+                                <a href="#list" data-toggle="tab"><i class="entypo-menu"></i> 
+                                    Admission Type List
+                                </a></li>
+                            <li>
+                                <a href="#add" data-toggle="tab"><i class="entypo-plus-circled"></i>
+                                    Add Admission Type
+                                </a></li>
+                        </ul>
+                        <!------CONTROL TABS END------>
+
+                        <div class="tab-content">
+                            <!----TABLE LISTING STARTS-->
+                            <div class="tab-pane box active" id="list">
+
+                                <div class="panel-body table-responsive">
+                                    <table class="table table-striped" id="data-tables">
+                                        <thead>
+                                            <tr>
+                                                <th><div>#</div></th>
+                                                <th>Admission Type Name</th>
+                                                <th>Status</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php $count = 1;
+                                            foreach ($admission_type as $row): ?>
+                                                <tr>
+                                                    <td><?php echo $count++; ?></td>
+                                                    <td><?php echo $row['at_name']; ?></td>                         
+                                                    <td class="text-center">
+                                                        <?php if ($row['at_status'] == '1') { ?>
+                                                            <span class="label label-success">Active</span>
+                                                        <?php } else { ?>	
+                                                            <span class="label label-default">InActive</span>
+    <?php } ?>
+                                                    </td>
+                                                    <td class="menu-action">
+                                                        <a href="#" onclick="showAjaxModal('<?php echo base_url(); ?>index.php?modal/popup/modal_edit_admission_type/<?php echo $row['at_id']; ?>');" data-original-title="edit" data-toggle="tooltip" data-placement="top" class="btn menu-icon vd_bd-yellow vd_yellow"><i class="fa fa-pencil"></i></a>
+
+                                                        <a href="#" onclick="confirm_modal('<?php echo base_url(); ?>index.php?admin/admission_type/delete/<?php echo $row['at_id']; ?>');" data-original-title="Remove" data-toggle="tooltip" data-placement="top" class="btn menu-icon vd_bd-red vd_red"><i class="fa fa-times"></i> </a>
+                                                    </td>
+                                                </tr>
+<?php endforeach; ?>						
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <!----TABLE LISTING ENDS--->
+
+
+                            <!----CREATION FORM STARTS---->
+                            <div class="tab-pane box" id="add" style="padding: 5px">
+                                <div class="box-content">  
+                                    <div class="">
+                                        <span style="color:red">* is mandatory field</span> 
+                                    </div>                                                                    
+                                        <?php echo form_open(base_url() . 'index.php?admin/admission_type/create', array('class' => 'form-horizontal form-groups-bordered validate', 'role' => 'form', 'id' => 'frmadmission_type', 'target' => '_top')); ?>
+                                    <div class="padded">
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label">Admission Type Name<span style="color:red">*</span></label>
+                                            <div class="col-sm-5">
+                                                <input type="text" class="form-control" name="at_name" id="at_name" />
+                                            </div>
+                                        </div>												
+                                        <div class="form-group">
+                                            <label class="col-sm-3 control-label">Status</label>
+                                            <div class="col-sm-5">
+                                                <select name="at_status">
+                                                    <option value="1">Active</option>
+                                                    <option value="0">Inactive</option>		
+                                                </select>	
+
+                                            </div>	
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="col-sm-offset-3 col-sm-5">
+                                                <button type="submit" class="btn btn-info vd_bg-green">Add Admission Type</button>
+                                            </div>
+                                        </div>
+                                        </form>               
+                                    </div>                
+                                </div>
+                                <!----CREATION FORM ENDS-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>              
+        </div>
+        <!-- row --> 
+    </div>
+    <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.js"></script>
+    <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.validate.min.js"></script>
+    <script type="text/javascript">
+
+    $().ready(function () {
+        $("#frmadmission_type").validate({
+            rules: {
+                at_name: "required",
+                at_name:
+                        {
+                            required: true,
+                            remote: {
+                                url: "<?php echo base_url() . 'index.php?admin/check_admission_type'; ?>",
+                                type: "post",
+                                data: {
+                                    admission_type: function () {
+                                        return $("#at_name").val();
+                                    },
+                                }
+                            }
+                        },
+                at_status: "required",
+            },
+            messages: {
+                at_name:
+                        {
+                            required: "Enter admission type name",
+                            remote: "Record is already present in the system",
+                        },
+                at_status: "Please slect admission status",
+            }
+        });
+    });
+    </script>
