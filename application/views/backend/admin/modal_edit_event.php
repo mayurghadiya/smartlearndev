1b7c2b7562a2a51f55fb263dfc0ad74f0d9cea61
mayur <mayur@localhost>
2016-05-04 15:14:29 +0530

    select value empty for event

--- application/views/backend/admin/modal_edit_event.php
+++ application/views/backend/admin/modal_edit_event.php
@@ -64,7 +64,7 @@ $edit_data = $this->db->get_where('event_manager', array('event_id' => $param2))
                             <?php
                             $group = $this->db->get('group')->result();
                             ?>
-                            <option>Select</option>
+                            <option value="">Select</option>
                             <?php foreach ($group as $gp) { ?>
                                 <option value="<?php echo $gp->g_id; ?>"
                                         <?php if ($gp->g_id == $row['group_id']) echo 'selected'; ?>><?php echo $gp->group_name; ?></option>
