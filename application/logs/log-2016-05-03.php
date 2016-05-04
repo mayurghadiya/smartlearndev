02db3fc86666e143aa034c9dd89e8296b602b73e
mayur <mayur@localhost>
2016-05-04 10:06:16 +0530

    pull

---
+++ application/logs/log-2016-05-03.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-03 19:20:27 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-03 19:20:31 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-03 19:20:32 --> 404 Page Not Found: Student/mainfest.appcache
+ERROR - 2016-05-03 19:21:49 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-03 19:21:58 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-03 19:21:58 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-03 19:21:58 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-03 19:21:59 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-03 19:22:24 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-03 19:22:25 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-03 19:22:25 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-03 19:22:37 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 19:22:48 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-03 19:23:32 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 19:25:20 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 19:25:23 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-03 19:25:58 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-03 19:26:08 --> 404 Page Not Found: Admin/assets
---
+++ application/logs/log-2016-05-04.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-04 10:00:46 --> 404 Page Not Found: Admin/img
+ERROR - 2016-05-04 10:00:47 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 10:00:59 --> 404 Page Not Found: Img/ico
+ERROR - 2016-05-04 10:01:01 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 10:01:01 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 10:01:01 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 10:01:01 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 10:01:08 --> 404 Page Not Found: Media/img
+ERROR - 2016-05-04 10:01:09 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-04 10:01:10 --> 404 Page Not Found: Media/assets
+ERROR - 2016-05-04 10:01:10 --> 404 Page Not Found: Media/uploads
+ERROR - 2016-05-04 10:01:18 --> 404 Page Not Found: Media/uploads
+ERROR - 2016-05-04 10:01:27 --> 404 Page Not Found: Media/uploads
+ERROR - 2016-05-04 10:01:27 --> 404 Page Not Found: Media/uploads
+ERROR - 2016-05-04 10:01:30 --> 404 Page Not Found: Media/uploads
+ERROR - 2016-05-04 10:01:51 --> 404 Page Not Found: Media/uploads
+ERROR - 2016-05-04 10:01:58 --> 404 Page Not Found: Media/uploads
+ERROR - 2016-05-04 10:02:15 --> 404 Page Not Found: Media/uploads
