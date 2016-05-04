dff2fb7b7a810615e82d81256d91b385365d9dea
mayur <mayur@localhost>
2016-05-04 12:23:10 +0530

    import

---
+++ application/logs/log-2016-05-04.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-04 12:21:37 --> Severity: Notice --> Only variables should be assigned by reference /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 2916
+ERROR - 2016-05-04 12:21:49 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:22:33 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:22:46 --> 404 Page Not Found: Media/mainfest.appcache
