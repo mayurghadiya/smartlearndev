56ee48c62ff42ce60309e23da5b19eb90dcf1afc
mayur <mayur@localhost>
2016-05-04 13:56:00 +0530

    custom css

---
+++ application/logs/log-2016-05-04.php
@@ -0,0 +1,119 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-04 12:37:51 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:37:57 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:37:59 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:38:04 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:38:05 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:38:10 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:38:10 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:38:23 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:38:47 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:39:12 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:39:18 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:40:58 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:41:31 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:45:37 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:46:46 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:46:58 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:47:13 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 4103
+ERROR - 2016-05-04 12:47:13 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/smartlearndev/application/controllers/Admin.php 4114
+ERROR - 2016-05-04 12:47:33 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:48:03 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:51:30 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:51:31 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:51:34 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:51:41 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 12:51:41 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 12:51:45 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:51:49 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:52:20 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:52:21 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:54:14 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 12:54:18 --> 404 Page Not Found: Img/logo.png
+ERROR - 2016-05-04 12:54:28 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 12:54:28 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 12:54:29 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 12:55:52 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 12:55:52 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 12:55:58 --> 404 Page Not Found: Img/ico
+ERROR - 2016-05-04 12:56:05 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 12:56:06 --> 404 Page Not Found: Admin/img
+ERROR - 2016-05-04 12:56:09 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-04 12:56:32 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 12:56:40 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:56:40 --> Severity: Notice --> Trying to get property of non-object /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:56:45 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 12:56:45 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 12:56:45 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 12:56:45 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 12:56:45 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 12:57:27 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 12:57:31 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:57:31 --> Severity: Notice --> Trying to get property of non-object /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:57:34 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 12:57:34 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 12:57:34 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 12:57:34 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 12:57:34 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 12:58:31 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Warning --> str_repeat() expects exactly 2 parameters, 3 given /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 55
+ERROR - 2016-05-04 12:58:34 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:58:34 --> Severity: Notice --> Trying to get property of non-object /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:58:37 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 12:58:37 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 12:58:37 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 12:58:37 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 12:59:16 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 12:59:19 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:59:19 --> Severity: Notice --> Trying to get property of non-object /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 12:59:22 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 12:59:22 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 12:59:22 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 12:59:22 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 12:59:22 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 13:00:51 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 13:00:54 --> Severity: Notice --> Undefined offset: 0 /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 13:00:54 --> Severity: Notice --> Trying to get property of non-object /opt/lampp/htdocs/smartlearndev/application/views/backend/student/gadgets/widget1.php 60
+ERROR - 2016-05-04 13:01:00 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 13:01:00 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 13:01:00 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 13:01:00 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 13:01:52 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 13:02:00 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 13:02:00 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 13:02:00 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 13:02:00 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 13:02:00 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 256
+ERROR - 2016-05-04 13:49:48 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-04 13:49:51 --> 404 Page Not Found: Media/img
+ERROR - 2016-05-04 13:50:16 --> 404 Page Not Found: Media/blank.gif
+ERROR - 2016-05-04 13:52:08 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-04 13:52:16 --> 404 Page Not Found: Media/img
+ERROR - 2016-05-04 13:52:25 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-04 13:52:26 --> 404 Page Not Found: Admin/img
+ERROR - 2016-05-04 13:53:09 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 13:53:09 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 13:53:09 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 13:53:09 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-04 13:53:30 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-04 13:53:31 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-04 13:53:31 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-04 13:53:44 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-04 13:54:15 --> 404 Page Not Found: Media/mainfest.appcache
+ERROR - 2016-05-04 13:55:00 --> 404 Page Not Found: Admin/blank.gif
+ERROR - 2016-05-04 13:55:16 --> 404 Page Not Found: Media/mainfest.appcache
