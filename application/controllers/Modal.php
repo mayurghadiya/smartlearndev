b22c2dc4a69ac776246cb154dec981b5af4f2d7e
mayur <mayur@localhost>
2016-05-04 11:23:50 +0530

    remedial exam schedual modal

--- application/controllers/Modal.php
+++ application/controllers/Modal.php
@@ -48,6 +48,22 @@ class Modal extends CI_Controller {
                     $page_data['semester'] = $this->Crud_model->get_all_semester();
                     //$page_data['centerlist'] = $this->db->get('center_user')->result(); 
                 }
+                if($page_name=="addremedial")
+                {
+                    $page_data['exams'] = $this->Crud_model->exam_details();
+                    $page_data['exam_type'] = $this->Crud_model->get_all_exam_type();
+                    $page_data['degree'] = $this->Crud_model->get_all_degree();
+                    $page_data['course'] = $this->Crud_model->get_all_course();
+                    $page_data['semester'] = $this->Crud_model->get_all_semester();
+                    //$page_data['centerlist'] = $this->db->get('center_user')->result(); 
+                }
+                if($page_name=="addremedial_schedual")
+                {
+                     $page_data['degree'] = $this->Crud_model->get_all_degree();
+                    $page_data['course'] = $this->Crud_model->get_all_course();
+                    $page_data['semester'] = $this->Crud_model->get_all_semester();
+                    $page_data['time_table'] = $this->Crud_model->time_table();
+                }
 		$this->load->view( 'backend/'.$account_type.'/'.$page_name.'.php' ,$page_data);		
 		echo '<script src="http://192.168.1.13/smart_learn_dev/assets/js/neon-custom-ajax.js"></script>';
 	}
