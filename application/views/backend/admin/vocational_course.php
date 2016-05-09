033bfda4ab0f5f299e0811c676efd7785cc0032e
nikita <mayur.ghadiya@searchnative.in>
2016-05-09 14:18:08 +0530

    listing vocational course

---
+++ application/views/backend/admin/vocational_course.php
@@ -0,0 +1,85 @@
+<!-- Middle Content Start -->    
+<div class="vd_content-wrapper">
+    <div class="vd_container">
+        <div class="vd_content clearfix">
+            <div class="vd_head-section clearfix">
+                <div class="vd_panel-header">
+                  <ul class="breadcrumb">
+                         <li><a href="<?php echo base_url('index.php?admin/dashboard'); ?>"><?php echo ucwords("home");?></a> </li>
+                         <li><?php echo ucwords("basic management");?></li>
+                         <li><?php echo $page_title;?></li>
+                    </ul>               
+                </div>
+            </div>
+            <div class="vd_title-section clearfix">
+                <div class="vd_panel-header no-subtitle">
+                    <h1><?php echo ucwords("vocational course");?></h1>
+                </div>
+            </div>
+            <div class="vd_content-section clearfix">
+                <div class="row">
+                    <div class="col-sm-12">								
+                        <!------CONTROL TABS START------>
+                        <ul class="nav nav-tabs bordered">
+                            <li class="active">
+                                <a href="#list" data-toggle="tab"><i class="entypo-menu"></i> 
+                                    <?php echo ucwords("vocational course list");?>
+                                </a></li>
+                          
+                        </ul>
+                        <!------CONTROL TABS END------>
+                            
+                        <div class="tab-content">
+                            <!----TABLE LISTING STARTS-->
+                            <div class="tab-pane box active" id="list">
+                                
+                                <div class="panel-body table-responsive">
+                                    <table class="table table-striped" id="data-tables">
+                                        <thead>
+                                            <tr>
+                                                <th><div>#</div></th>
+                                                <th><?php echo ucwords("course name");?></th>
+                                                <th><?php echo ucwords("course start date");?></th>
+                                                <th><?php echo ucwords("course end date");?></th>
+                                                <th><?php echo ucwords("course fee");?></th>
+                                                <th><?php echo ucwords("professor name");?></th>
+                                                <th><?php echo ucwords("status");?></th>
+                                                <th><?php echo ucwords("action");?></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+  
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <!----TABLE LISTING ENDS--->
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>              
+        </div>
+        <!-- row --> 
+    </div>
+     <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.js"></script>
+    <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.validate.min.js"></script>
+    
+<style>
+    .nav-fixedtabs {
+    left: 86%;
+    position: fixed;
+    top: 25%;
+    }
+    #navfixed{
+        cursor: pointer;
+    }
+    
+    </style>
+    
+    <div class="md-fab-wrapper">
+        <a class="md-fab md-fab-success nav-fixed-a-tabs vd_bg-red"  onclick="showAjaxModal('<?php echo base_url(); ?>index.php?modal/popup/addvocationalcourse/');" href="#" id="navfixed" data-toggle="tab">
+            <i class="material-icons">&#xE145;</i>
+        </a>
+    </div>
+   
