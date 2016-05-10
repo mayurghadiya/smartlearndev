2626e1f9caf31c89e97e84ebab071f06bccaf33a
nikita <mayur.ghadiya@searchnative.in>
2016-05-10 18:25:28 +0530

    class CURD

--- application/views/backend/admin/class.php
+++ application/views/backend/admin/class.php
@@ -1,203 +1,97 @@
-<div class="row">
-	<div class="col-md-12">
-    
-    	<!------CONTROL TABS START------>
-		<ul class="nav nav-tabs bordered">
-			<li class="active">
-            	<a href="#list" data-toggle="tab"><i class="entypo-menu"></i> 
-					<?php echo get_phrase('class_list');?>
-                </a>
-			</li>
-			<li>
-            	<a href="#add" data-toggle="tab"><i class="entypo-plus-circled"></i>
-					<?php echo get_phrase('add_class');?>
-                </a>
-			</li>
-		</ul>
-    	<!------CONTROL TABS END------>
-        
-		<div class="tab-content">
-            <!----TABLE LISTING STARTS-->
-            <div class="tab-pane box active" id="list">				
-                <table class="table table-bordered datatable" id="table_export">
-                	<thead>
-                		<tr>
-                    		<th><div>#</div></th>
-                    		<th><div>Class Name</div></th>
-                    		<th><div>Teacher</div></th>                    		
-                    		<th><div><?php echo get_phrase('options');?></div></th>
-						</tr>
-					</thead>
-                    <tbody>
-                    	<?php $count = 1;foreach($classes as $row):?>
-                        <tr>
-                            <td><?php echo $count++;?></td>
-							<!--<td><?php echo $row['name'];?></td>
-							<td><?php echo $row['std'];?></td>
-							<td><?php echo $row['division'];?></td>	-->	
-							<td><?php echo $row['name_numeric'];?></td>								
-							<td><?php echo $this->crud_model->get_type_name_by_id('teacher',$row['teacher_id']);?>&nbsp;<?php								
-								if(!empty($this->crud_model->get_teacher_name_by_id('teacher',$row['teacher_id']))){ ?>
-								<a href="download.php?file_name=<?php echo $this->crud_model->get_teacher_name_by_id('teacher',$row['teacher_id']);?>" class="links"><i class="fa fa-download"></i></a>	
-								<?php }	?>
-							</td>
-							<td>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown">
-                                    Action <span class="caret"></span>
-                                </button>
-                                <ul class="dropdown-menu dropdown-default pull-right" role="menu">
-                                    
-                                    <!-- EDITING LINK -->
-                                    <li>
-                                        <a href="#" onclick="showAjaxModal('<?php echo base_url();?>index.php?modal/popup/modal_edit_class/<?php echo $row['class_id'];?>');">
-                                            <i class="entypo-pencil"></i>
-                                                <?php echo get_phrase('edit');?>
-                                            </a>
-                                                    </li>
-                                    <li class="divider"></li>
-                                    
-                                    <!-- DELETION LINK -->
-                                    <li>
-                                        <a href="#" onclick="confirm_modal('<?php echo base_url();?>index.php?admin/classes/delete/<?php echo $row['class_id'];?>');">
-                                            <i class="entypo-trash"></i>
-                                                <?php echo get_phrase('delete');?>
-                                            </a>
-                                    </li>
-                                </ul>
-                            </div>
-        					</td>
-                        </tr>
-                        <?php endforeach;?>
-                    </tbody>
-                </table>
-			</div>
-            <!----TABLE LISTING ENDS--->
-            
-            
-			<!----CREATION FORM STARTS---->
-			<div class="tab-pane box" id="add" style="padding: 5px">
-                <div class="box-content">
-                	<?php echo form_open(base_url() . 'index.php?admin/classes/create' , array('class' => 'form-horizontal form-groups-bordered validate','target'=>'_top'));?>
-                        <div class="padded">
-                            <div class="form-group">
-                                <label class="col-sm-3 control-label">Class Name</label>
-                                <div class="col-sm-5">
-                                    <input type="text" class="form-control" name="name" data-validate="required" data-message-required="<?php echo get_phrase('value_required');?>"/>
+
+<!-- Middle Content Start -->    
+<div class="vd_content-wrapper">
+    <div class="vd_container">
+        <div class="vd_content clearfix">
+             <div class="vd_head-section clearfix">
+                <div class="vd_panel-header">
+                    <ul class="breadcrumb">                      
+                         <li><a href="<?php echo base_url('index.php?admin/dashboard'); ?>"><?php echo ucwords("home");?></a> </li>
+                         <li><?php echo ucwords("basic management");?></li>
+                         <li><?php echo ucwords("class");?></li>
+                        
+                    </ul>                  
+                </div>
+            </div>
+            <div class="vd_title-section clearfix">
+                <div class="vd_panel-header no-subtitle">
+                    <h1><?php echo ucwords("class management");?></h1>
+                </div>
+            </div>
+            <div class="vd_content-section clearfix">
+                <div class="row">
+                    <div class="col-sm-12">								
+                        <!------CONTROL TABS START------>
+                        <ul class="nav nav-tabs bordered">
+                            <li class="active">
+                                <a href="#list" data-toggle="tab"><i class="entypo-menu"></i> 
+                                   <?php echo ucwords(" class list");?>
+                                </a></li>
+                           
+                        </ul>
+                        <!------CONTROL TABS END------>
+
+                        <div class="tab-content">
+                            <!----TABLE LISTING STARTS-->
+                            <div class="tab-pane box active" id="list">
+
+                                <div class="panel-body table-responsive">
+                                    <table class="table table-striped table-responsive" id="data-tables">
+                                        <thead>
+                                            <tr>
+                                                <th><div>#</div></th>
+                                                <th><?php echo ucwords("class name");?></th>
+                                                <th><?php echo ucwords("action");?></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php $count = 1;
+                                            foreach ($class as $row): ?>
+                                                <tr>
+                                                    <td><?php echo $count++; ?></td>
+                                                    <td><?php echo $row['class_name']; ?></td>                         
+                                                    <td class="menu-action">
+                                                        <a href="#" onclick="showAjaxModal('<?php echo base_url(); ?>index.php?modal/popup/modal_edit_class/<?php echo $row['class_id']; ?>');" data-original-title="edit" data-toggle="tooltip" data-placement="top" class="btn menu-icon vd_bd-yellow vd_yellow"><i class="fa fa-pencil"></i></a>
+
+                                                        <a href="#" onclick="confirm_modal('<?php echo base_url(); ?>index.php?admin/division/delete/<?php echo $row['class_id']; ?>');" data-original-title="Remove" data-toggle="tooltip" data-placement="top" class="btn menu-icon vd_bd-red vd_red"><i class="fa fa-times"></i> </a>
+                                                    </td>
+                                                </tr>
+<?php endforeach; ?>						
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
-							<div id="drop">
-								<div class="form-group">
-									<label class="col-sm-3 control-label">Std</label>
-									<div class="col-sm-5">
-										<select style="width:100%;" class="form-control" name="std" id="dpstd">
-												<option value="">Select Standard </option>
-												<option value="1">1 </option>
-												<option value="2">2 </option>
-												<option value="3">3 </option>
-												<option value="4">4 </option>
-												<option value="5">5 </option>
-												<option value="6">6 </option>
-												<option value="7">7 </option>
-												<option value="8">8 </option>
-												<option value="9">9 </option>
-												<option value="10">10 </option>
-												<option value="11">11 </option>
-												<option value="12">12 </option>
-										</select>
-									</div>
-								</div>
-								<div class="form-group">
-									<label class="col-sm-3 control-label">Division</label>
-									<div class="col-sm-5">
-										<select style="width:100%;" class="form-control" name="division" id="dpdiv">
-												<option value="">Select Division </option>
-												<option value="A">A </option>
-												<option value="B">B </option>
-												<option value="C">C </option>
-												<option value="D">D </option>
-												<option value="E">E </option>
-												<option value="F">F </option>
-												<option value="G">G </option>
-												<option value="H">H </option>
-												<option value="I">I </option>
-												<option value="J">J </option>											
-										</select>
-									</div>
-								</div>
-							</div>
-							<div class="form-group">
-                                <label class="col-sm-3 control-label"><?php echo get_phrase('teacher');?></label>
-                                <div class="col-sm-5">
-                                    <select name="teacher_id" class="form-control" style="width:100%;">
-                                    	<?php 
-										$teachers = $this->db->get('teacher')->result_array();
-										foreach($teachers as $row):
-										?>
-                                    		<option value="<?php echo $row['teacher_id'];?>"><?php echo $row['name'];?></option>
-                                        <?php
-										endforeach;
-										?>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-sm-3 control-label">ID</label>
-                                <div class="col-sm-5">
-                                   <div class="detail">
-										<input type="text" name="name_numeric" id="showoption" disabled="disabled">
-								   </div>
-								    <script type="text/javascript">
-										$('#drop').change(
-											function() {
-												var val1 = $('#dpstd option:selected').val();
-												var val2 = $('#dpdiv option:selected').val();
-												var total =val1 + val2;
-												if(val1 != '' && val2 != ''){
-													$('.detail').find(':input').val(total);	
-												}			
-													// Do something with val1 and val2 ...
-											}
-										);
-									</script>
-								      
-								   <?php //echo $row['name_numeric'];?></LABEL>
-                                </div>
-                            </div>                           
+                            <!----TABLE LISTING ENDS--->
+
+
+                           
                         </div>
-                        <div class="form-group">
-                              <div class="col-sm-offset-3 col-sm-5">
-                                  <button type="submit" class="btn btn-info"><?php echo get_phrase('add_class');?></button>
-                              </div>
-						</div>
-                    </form>                
-                </div>                
-			</div>
-			<!----CREATION FORM ENDS-->
-		</div>
-	</div>
-</div>
+                    </div>
+                </div>
+            </div>              
+        </div>
+        <!-- row --> 
+    </div>
+    <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.js"></script>
+    <script type="text/javascript" src="<?= $this->config->item('js_path') ?>jquery.validate.min.js"></script>
+    
+    
+      <style>
+    .nav-fixedtabs {
+    left: 86%;
+    position: fixed;
+    top: 25%;
+    }
+    #navfixed{
+        cursor: pointer;
+    }
+    
+    </style>
+    
+  
+    <div class="md-fab-wrapper">
 
-
-
-<!-----  DATA TABLE EXPORT CONFIGURATIONS ---->                      
-<script type="text/javascript">
-
-	jQuery(document).ready(function($)
-	{	
-
-		var datatable = $("#table_export").dataTable();
-		
-		$(".dataTables_wrapper select").select2({
-			minimumResultsForSearch: -1
-		});
-		
-		$('#example').dataTable( {
-				paging: false,
-				searching: false
-		} );
-		
-	});
-		
-</script>
-<?php include('plus_icon.php'); ?>
+        <a class="md-fab md-fab-success nav-fixed-a-tabs vd_bg-red"  onclick="showAjaxModal('<?php echo base_url(); ?>index.php?modal/popup/addclass/');" href="#" id="navfixed" data-toggle="tab">
+            <i class="material-icons">&#xE145;</i>
+        </a>
+    </div>
