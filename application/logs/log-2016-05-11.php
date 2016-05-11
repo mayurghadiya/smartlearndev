009047bdb2b57fd9b4039cef9a12a229bb745eb6
mayur <mayur@localhost>
2016-05-11 15:14:23 +0530

    admin comment

---
+++ application/logs/log-2016-05-11.php
@@ -0,0 +1,31 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-11 13:40:18 --> Severity: Core Warning --> PHP Startup: Unable to load dynamic library '/opt/lampp/lib/php/extensions/no-debug-non-zts-20151012/pdo_mysql.so' - /opt/lampp/lib/php/extensions/no-debug-non-zts-20151012/pdo_mysql.so: cannot open shared object file: No such file or directory Unknown 0
+ERROR - 2016-05-11 13:40:18 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-11 13:40:18 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-11 13:40:23 --> 404 Page Not Found: Img/ico
+ERROR - 2016-05-11 13:40:25 --> 404 Page Not Found: Forum/mainfest.appcache
+ERROR - 2016-05-11 13:40:26 --> 404 Page Not Found: Forum/img
+ERROR - 2016-05-11 13:45:46 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-11 13:45:47 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-11 13:45:52 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-11 13:45:54 --> 404 Page Not Found: Admin/assets
+ERROR - 2016-05-11 13:46:22 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-11 13:47:14 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 257
+ERROR - 2016-05-11 13:47:15 --> 404 Page Not Found: Img/logo.png
+ERROR - 2016-05-11 13:47:15 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-11 13:47:15 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-11 13:47:15 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-11 13:47:15 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-11 13:49:24 --> Severity: Notice --> Undefined variable: gochat /opt/lampp/htdocs/smartlearndev/application/views/backend/student/includes/header.php 257
+ERROR - 2016-05-11 13:49:27 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-11 13:49:27 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-11 13:49:27 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-11 14:21:19 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-11 14:21:19 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-11 15:01:12 --> 404 Page Not Found: Indexhtml/index
+ERROR - 2016-05-11 15:01:12 --> 404 Page Not Found: Logopng/index
+ERROR - 2016-05-11 15:01:12 --> 404 Page Not Found: No-internethtml/index
+ERROR - 2016-05-11 15:01:13 --> 404 Page Not Found: Admin/mainfest.appcache
+ERROR - 2016-05-11 15:01:14 --> 404 Page Not Found: Stylecss/index
+ERROR - 2016-05-11 15:01:14 --> Severity: Core Warning --> PHP Startup: Unable to load dynamic library '/opt/lampp/lib/php/extensions/no-debug-non-zts-20151012/pdo_mysql.so' - /opt/lampp/lib/php/extensions/no-debug-non-zts-20151012/pdo_mysql.so: cannot open shared object file: No such file or directory Unknown 0
